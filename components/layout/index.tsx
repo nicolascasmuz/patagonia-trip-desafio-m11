a37645b53806058da0946231aabdded605093434
lucasfrezzini <luquiitas.f@gmail.com>
2025-03-19 02:24:13 -0300

    Refactor: edit CSS Globals, import new font, some styles, remove unused css from next bootstrap

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,17 @@
-import { Footer } from "components/footer";
-import { Header } from "components/header";
+import Footer from "components/footer";
+import Header from "components/header";
 import styles from "./layout.module.css";
 
 export default function Layout({ children }: any) {
   return (
     <div className={styles.div}>
-      <Header />
-      <div>{children}</div>
-      <Footer />
+      <div className="container">
+        <Header />
+      </div>
+      <div className="container">{children}</div>
+      <div className="container">
+        <Footer />
+      </div>
     </div>
   );
 }

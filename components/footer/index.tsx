f59bc16f9b79d5268c34fd09fd47069b5b10e999
lucasfrezzini <luquiitas.f@gmail.com>
2025-03-19 04:48:57 -0300

    Feat: add footer component with logo and social media links

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,5 +1,9 @@
 import styles from "./footer.module.css";
 import Image from "next/image";
+import logotipo from "../../resources/logo-patagonia-trip.png";
+import facebook from "../../resources/facebook-icon.webp";
+import instagram from "../../resources/instagram-icon.webp";
+import twitter from "../../resources/twitter-icon.webp";
 
 // MODO DE APLICAR CLASES A LAS ETIQUETAS:
 // className={styles["nombre-de-la-clase"]}
@@ -25,3 +29,44 @@ export default function Component() {
 }
 
 */
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <Image
+        src={logotipo}
+        alt="Patagonia Trip Logo"
+        className={styles["proyect-logo"]}
+      />
+      <ul className={styles["socials"]}>
+        <li>
+          <a href="#" target="_blank">
+            <Image
+              src={instagram}
+              alt="Patagonia Trip Logo"
+              className={styles["proyect-logo"]}
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank">
+            <Image
+              src={twitter}
+              alt="Patagonia Trip Logo"
+              className={styles["proyect-logo"]}
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank">
+            <Image
+              src={facebook}
+              alt="Patagonia Trip Logo"
+              className={styles["proyect-logo"]}
+            />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}

f0a2b4de3ddc34c11aa60512d1f58d34733aa09b
Nicolás Casmuz <nicolascasmuz@gmail.com>
2025-10-27 21:31:53 -0300

    commit-016

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
 const BASE_URL = "https://patagonia-trip-backend.vercel.app/api";
+// const BASE_URL = "http://localhost:3000/api";
 
 export async function fetchAPI(input?: RequestInfo, options?) {
   const url = BASE_URL + input;
